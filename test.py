d87d1df7ecb44a58e1d9268ae9c914156405bbce
ZCY01 <292695903@qq.com>
2019-06-01 15:28:51 +0800

    借口增加指定翻译的语言类型,当翻译语言未指定时才去调用langdetect

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     d = BaiduTranslate.Dict()
     # open fanyi.baidu.com to get the code of language
     # https://fanyi.baidu.com/#zh/en/你好
-    json = d.dictionary('like a hot knife through butter', src='en', dst='zh')
+    json = d.dictionary('like a hot knife through butter', dst='zh', src='en')
     print(json)
-    json = d.dictionary('青花瓷', src='zh', dst='en')
-    print(json)
+    json = d.dictionary('青花瓷', dst='en', src='zh')
+    print(json['trans_result']['data'][0]['dst'])

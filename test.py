6cae4e9d822b0dcd487fc81ce57c66f5713878a6
cheerz <cheerz@domain.com>
2019-06-01 15:38:19 +0800

    修改接口参数，提供参数指定翻译语言

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import BaiduTranslate
 
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     json = d.dictionary('like a hot knife through butter', dst='zh', src='en')
     print(json)
     json = d.dictionary('青花瓷', dst='en', src='zh')
-    print(json['trans_result']['data'][0]['dst']) 
+    print(json['trans_result']['data'][0]['dst'])

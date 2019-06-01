22e6f0c6b89f0e97aa2aa0ef0356dcc2af9ec02b
cheerz <cheerz@domain.com>
2019-06-01 15:25:16 +0800

    借口增加指定翻译的语言类型,当翻译语言未指定时才去调用langdetect

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
 import BaiduTranslate
 
 if __name__ == "__main__":
     d = BaiduTranslate.Dict()
-    json = d.dictionary('hello')
+    # open fanyi.baidu.com to get the code of language
+    # https://fanyi.baidu.com/#zh/en/你好
+    json = d.dictionary('like a hot knife through butter', src='en', dst='zh')
+    print(json)
+    json = d.dictionary('青花瓷', src='zh', dst='en')
     print(json)
